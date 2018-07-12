bc5eaf390a6a86565e7e7c1cb9c1036dc2948ad0
Anh Tuan <tincanxh@gmail.com>
2018-07-12 16:54:06 +0700

    tinh tong cac so trong file text

--- src/main/java/calculateSumFileText/SumNumbersInTheTextFile.java
+++ src/main/java/calculateSumFileText/SumNumbersInTheTextFile.java
@@ -1,4 +1,29 @@
 package calculateSumFileText;
 
+import java.io.*;
+
 public class SumNumbersInTheTextFile {
+    public static void main(String[] args) {
+        SumNumbersInTheTextFile sumNumbersInTheTextFile = new SumNumbersInTheTextFile();
+        sumNumbersInTheTextFile.readingFileText();
+    }
+
+    private void readingFileText() {
+        try {
+            File file = new File("./src/main/resources/data");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedInputStream = new BufferedReader(fileReader);
+            String line = "";
+            int sum = 0;
+            while ((line = bufferedInputStream.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            System.out.println("Sum: " + sum);
+        } catch (FileNotFoundException e) {
+            System.out.println("not find file text");
+        } catch (IOException e) {
+            System.out.println("no");
+        }
+    }
 }
